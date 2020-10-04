7717f445a8ae53bf94ac9fb89c0f8e9a4aa2194a
Alex Ionescu <alxi.2001@gmail.com>
2020-10-04 17:38:52 +0300

    Add --version global option

--- src/Language/OpLang/Opts.hs
+++ src/Language/OpLang/Opts.hs
@@ -26,21 +26,24 @@ defaultOutPath = ""
 optsParser :: ParserInfo Opts
 optsParser =
   info
-    (helper <*> programOptions)
+    (infoOption "oplang v0.1.0.0" (short 'v' <> long "version" <> help "Shows version information.")
+    <*> helper
+    <*> programOptions)
+    
     (fullDesc
-      <> progDesc "Compiles an OpLang source file to a native executable."
-      <> header "oplang - The OpLang Compiler")
+    <> progDesc "Compiles an OpLang source file to a native executable."
+    <> header "oplang - The OpLang Compiler")
 
   where
     programOptions :: Parser Opts
     programOptions =
       Opts
-        <$> option auto (short 'O' <> long "opt-passes" <> metavar "PASSES" <> value defaultOptPasses <> help "Specify the number of optimization passes to perform.")
-        <*> option auto (short 'S' <> long "stack-size" <> metavar "STACK" <> value defaultStackSize <> help "Specify the size of the stack.")
-        <*> option auto (short 'T' <> long "tape-size" <> metavar "TAPE" <> value defaultTapeSize <> help "Specify the size of the memory tape.")
-        <*> switch (short 'K' <> long "keep-c-file" <> help "Specifiy whether to keep the resulting C file.")
-        <*> strOption (short 'o' <> long "out-path" <> metavar "OUT_PATH" <> value defaultOutPath <> help "Specify the path of the resulting executable.")
-        <*> strArgument (metavar "PATH" <> help "The source file to compile.")
+      <$> option auto (short 'O' <> long "opt-passes" <> metavar "PASSES" <> value defaultOptPasses <> help "Specify the number of optimization passes to perform.")
+      <*> option auto (short 'S' <> long "stack-size" <> metavar "STACK" <> value defaultStackSize <> help "Specify the size of the stack.")
+      <*> option auto (short 'T' <> long "tape-size" <> metavar "TAPE" <> value defaultTapeSize <> help "Specify the size of the memory tape.")
+      <*> switch (short 'K' <> long "keep-c-file" <> help "Specifiy whether to keep the resulting C file.")
+      <*> strOption (short 'o' <> long "out-path" <> metavar "OUT_PATH" <> value defaultOutPath <> help "Specify the path of the resulting executable.")
+      <*> strArgument (metavar "PATH" <> help "The source file to compile.")
 
 getOpts :: IO Opts
 getOpts = execParser optsParser
