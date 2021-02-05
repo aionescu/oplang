8f9c0ce9b81df2e2f8cf1676d4f36c4ce1e14533
Alex Ionescu <alxi.2001@gmail.com>
2021-02-06 01:24:50 +0200

    Refactor Parser

--- src/Language/OpLang/Parser.hs
+++ src/Language/OpLang/Parser.hs
@@ -1,14 +1,16 @@
 module Language.OpLang.Parser(parse) where
 
-import Data.Text(Text)
 import Data.Bifunctor(first)
 import Data.Functor(($>))
-import Control.Applicative(liftA2)
+import Data.Text(Text)
 import Text.Parsec hiding (parse)
 
 import Language.OpLang.IR
 
 type Parser = Parsec Text ()
+
+reserved :: [Char]
+reserved = "+-<>,.;:[]{}# \t\r\n"
 
 comment :: Parser ()
 comment = char '#' *> manyTill anyChar (endOfLine $> () <|> eof) $> ()
@@ -17,7 +19,8 @@ ws :: Parser ()
 ws = spaces *> skipMany (comment *> spaces)
 
 intrinsic :: Parser Op
-intrinsic = choice
+intrinsic =
+  choice
   [ char '+' $> Add 1
   , char '-' $> Add (-1)
   , char '<' $> Move (-1)
@@ -29,27 +32,25 @@ intrinsic = choice
   ]
 
 block :: Char -> Char -> Parser [Op]
-block b e = between (char b *> ws) (char e *> ws) $ many (op <* ws)
+block b e = between (char b *> ws) (char e *> ws) $ many op
 
 loop :: Parser Op
 loop = Loop <$> block '[' ']'
 
 custom :: Parser Char
 custom = noneOf reserved
-  where
-    reserved = "+-<>,.;:[]{}# \t\r\n"
 
 op :: Parser Op
-op = choice [try loop, intrinsic, OpCall . Just <$> custom]
+op = choice [try loop, intrinsic, OpCall . Just <$> custom] <* ws
 
 def :: Parser Def
-def = liftA2 ((,) . Just) (custom <* ws) (block '{' '}')
+def = (,) . Just <$> (custom <* ws) <*> block '{' '}'
 
 topLevel :: Parser Def
-topLevel = (Nothing,) <$> many (op <* ws)
+topLevel = (Nothing,) <$> many op
 
 program :: Parser [Def]
-program = ws *> liftA2 (flip (:)) (many $ try def) topLevel <* eof
+program = ws *> (flip (:) <$> many (try def) <*> topLevel) <* eof
 
 parse :: Text -> Either String [Def]
 parse = first show . runParser program () ""
