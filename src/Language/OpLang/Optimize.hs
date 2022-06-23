98a26326ed68e6c3137b29ab6533dc005b493ab3
Alex Ionescu <alxi.2001@gmail.com>
2022-06-23 21:10:39 +0300

    Perform all optimizations in one pass

--- src/Language/OpLang/Optimize.hs
+++ src/Language/OpLang/Optimize.hs
@@ -1,97 +1,51 @@
 module Language.OpLang.Optimize(optimize) where
 
-import Control.Monad.ST(ST, runST)
-import Data.Foldable(fold)
-import Data.Functor(($>))
-import Data.STRef(STRef, readSTRef, newSTRef, writeSTRef, modifySTRef')
-
 import Language.OpLang.IR
 
-toInstr :: Op -> Instr
-toInstr = \case
-  Incr -> Add 1 0
-  Decr -> Add -1 0
-  MoveL -> Move -1
-  MoveR -> Move 1
-  Read' -> Read 0
-  Write' -> Write 0
-  Pop' -> Pop 0
-  Push' -> Push 0
-  Loop' ops -> Loop $ toInstr <$> ops
-  Call' c -> Call c
+syncAdd :: Bool -> Val -> Offset -> [Instr] -> [Instr]
+syncAdd False 0 _ acc = acc
+syncAdd False n offset acc = Add n offset : acc
+syncAdd True n offset acc = Set n offset : acc
 
-peephole :: [Instr] -> [Instr]
-peephole = go []
-  where
-    go :: [Instr] -> [Instr] -> [Instr]
-    go acc = \case
-      Add 0 _ : ops -> go acc ops
-      Move 0 : ops -> go acc ops
-
-      Loop [Add -1 _] : ops -> go acc (Set 0 0 : ops)
-      Loop [Add 1 _] : ops -> go acc (Set 0 0 : ops)
-
-      Set s _ : Add a _ : ops -> go acc (Set (s + a) 0 : ops)
-      Add a _ : Add b _ : ops -> go acc (Add (a + b) 0 : ops)
-      Move a : Move b : ops -> go acc (Move (a + b) : ops)
-
-      Add{} : ops@(Read{} : _) -> go acc ops
-      Add{} : ops@(Pop{} : _) -> go acc ops
-      Add{} : ops@(Set{} : _) -> go acc ops
-
-      Set{} : ops@(Read{} : _) -> go acc ops
-      Set{} : ops@(Pop{} : _) -> go acc ops
-      Set{} : ops@(Set{} : _) -> go acc ops
-
-      Set 0 _ : Loop _ : ops -> go acc (Set 0 0 : ops)
-      l@(Loop _) : Loop _ : ops -> go acc (l : ops)
-      Loop [l@(Loop _)] : ops -> go acc (l : ops)
-      Loop l : ops -> go (Loop (peephole l) : acc) ops
-
-      op : ops -> go (op : acc) ops
-      [] -> reverse acc
-
-removeSet0 :: [Instr] -> [Instr]
-removeSet0 (Set 0 _ : ops) = ops
-removeSet0 ops = ops
-
-sync :: STRef s Offset -> ST s [Instr]
-sync r = readSTRef r >>= \case
-  0 -> pure []
-  n -> writeSTRef r 0 $> [Move n]
-
-withOffsetLoop :: STRef s Offset -> [Instr] -> ST s Instr
-withOffsetLoop r ops = (\a b -> mkLoop $ fold a <> b) <$> traverse (withOffset r) ops <*> sync r
-  where
-    mkLoop [Add n o, Add -1 0] = AddCell n o
-    mkLoop [Add -1 0, Add n o] = AddCell n o
-    mkLoop l = Loop l
-
-withOffset :: STRef s Offset -> Instr -> ST s [Instr]
-withOffset r op = readSTRef r >>= \off ->
-  case op of
-    Add n _ -> pure [Add n off]
-    Set n _ -> pure [Set n off]
-    Pop _ -> pure [Pop off]
-    Push _ -> pure [Push off]
-    Read _ -> pure [Read off]
-    Write _ -> pure [Write off]
-    Move n -> modifySTRef' r (+ n) $> []
-    Loop ops -> (\a b -> a <> [b]) <$> sync r <*> withOffsetLoop r ops
-    Call c -> pure [Call c]
-    AddCell{} -> error "withOffset: Unreacahble"
-
-withOffsetOps :: [Instr] -> [Instr]
-withOffsetOps ops = fold $ runST do
-  r <- newSTRef 0
-  traverse (withOffset r) ops
+syncMove :: Offset -> [Instr] -> [Instr]
+syncMove 0 acc = acc
+syncMove n acc = Move n : acc
 
 optimizeOps :: [Op] -> [Instr]
-optimizeOps ops = withOffsetOps $ removeSet0 $ peephole $ Set 0 0 : (toInstr <$> ops)
+optimizeOps = go False True 0 0 []
+  where
+    go :: Bool -> Bool -> Val -> Offset -> [Instr] -> [Op] -> [Instr]
+    go False _ _ _ acc [] = reverse acc
+    go True known tally offset acc [] = reverse $ syncAdd known tally 0 $ syncMove offset acc
+    go loop known tally offset acc (op : ops) =
+      case op of
+        Incr -> go loop known (tally + 1) offset acc ops
+        Decr -> go loop known (tally - 1) offset acc ops
+        Loop' [Incr] -> go loop True 0 offset acc ops
+        Loop' [Decr] -> go loop True 0 offset acc ops
+        Read' -> go loop False 0 offset (Read offset : acc) ops
+        Pop' -> go loop False 0 offset (Pop offset : acc) ops
+
+        MoveL -> go loop False 0 (offset - 1) acc' ops
+        MoveR -> go loop False 0 (offset + 1) acc' ops
+        Write' -> go loop False 0 offset (Write offset : acc') ops
+        Push' -> go loop False 0 offset (Push offset : acc') ops
+        Call' c -> go loop False 0 offset (Call c : acc') ops
+
+        Loop' l -> go loop False 0 0 (goLoop l : syncMove offset acc') ops
+      where
+        acc' = syncAdd known tally offset acc
+
+    goLoop :: [Op] -> Instr
+    goLoop ops =
+      case go True False 0 0 [] ops of
+        [Add n o, Add -1 0] -> AddCell n o
+        [Add -1 0, Add n o] -> AddCell n o
+        l -> Loop l
 
 optimize :: Program Op -> Program Instr
 optimize Program{..} =
   Program
-  { opDefs = optimizeOps  <$> opDefs
+  { opDefs = optimizeOps <$> opDefs
   , topLevel = optimizeOps topLevel
   }
