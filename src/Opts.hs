44dfdb8dcdd2fb3c62e1a954dbb94c66d01ee06e
Alex Ionescu <aaionescu@pm.me>
2023-12-28 01:44:44 +0100

    Improve `--help` text

--- src/Opts.hs
+++ src/Opts.hs
@@ -18,29 +18,22 @@ data Opts =
   , path :: FilePath
   }
 
-optsParser :: ParserInfo Opts
-optsParser =
-  info
-    (infoOption ("oplang v" <> ver) (short 'v' <> long "version" <> help "Shows version information.")
-      <*> helper
-      <*> programOptions)
-    (fullDesc
-      <> progDesc "Compiles an OpLang source file to a native executable."
-      <> header "oplang - The OpLang Compiler")
-  where
-    ver = showVersion version
-
-    programOptions :: Parser Opts
-    programOptions =
-      Opts
-      <$> option auto (long "stack-size" <> value 4096 <> metavar "SIZE" <> help "Size of the stack.")
-      <*> option auto (long "tape-size" <> value 4096 <> metavar "SIZE" <> help "Size of the memory tape.")
-      <*> switch (long "no-warn" <> help "Don't report warnings.")
-      <*> switch (long "no-cc" <> help "Output a C file without compiling it.")
-      <*> switch (long "dump-ast" <> help "Print the AST after parsing.")
-      <*> switch (long "dump-ir" <> help "Print the IR after optimization.")
-      <*> optional (strOption (short 'o' <> long "out-path" <> metavar "PATH" <> help "Path of the resulting executable (or C file if --no-cc is passed)."))
-      <*> strArgument (metavar "PATH" <> help "The source file to compile.")
-
 getOpts :: IO Opts
 getOpts = execParser optsParser
+  where
+    ver = "oplang v" <> showVersion version
+    verOpt = infoOption ver (short 'v' <> long "version" <> help "Show version information")
+
+    opts :: Parser Opts
+    opts =
+      Opts
+      <$> option auto (long "stack-size" <> value 4096 <> metavar "SIZE" <> help "Size of the stack, in bytes (default 4096)")
+      <*> option auto (long "tape-size" <> value 4096 <> metavar "SIZE" <> help "Size of the memory tape, in bytes (default 4096)")
+      <*> switch (long "no-warn" <> help "Don't report warnings")
+      <*> switch (long "no-cc" <> help "Output a C file without compiling it")
+      <*> switch (long "dump-ast" <> help "Print the AST after parsing")
+      <*> switch (long "dump-ir" <> help "Print the IR after optimization")
+      <*> optional (strOption $ short 'o' <> long "out-path" <> metavar "PATH" <> help "Path of the resulting executable (or C file if --no-cc is passed)")
+      <*> strArgument (metavar "PATH" <> help "The source file to compile")
+
+    optsParser = info (helper <*> verOpt <*> opts) (fullDesc <> header ver)
